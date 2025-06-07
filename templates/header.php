23b9e5f502f7c2080ac7951a189757aeae038361
Vanessa <vanessasilva.08m@gmail.com>
2025-06-07 08:32:11 -0300

    sua mensagem

---
+++ templates/header.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.css" integrity="sha512-drnvWxqfgcU6sLzAJttJv7LKdjWn0nxWCSbEAtxJ/YYaZMyoNLovG7lPqZRdhgL1gAUfa+V7tbin8y+2llC1cw==" crossorigin="anonymous" />
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="css/style.css"/>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+      <link rel="icon" href="../images/1.png" type="image/png">
+
+    <title>Portfólio</title>
+</head>
+<body>
+
+
+<nav class="navbar navbar-expand-lg">
+    <div  class="collapse navbar-collapse"><a id="a-menu" href="index.php"><img id="foto" src="images/perfil.png" alt="">VanessaMSilva</a></div>
+  
+  
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav center" id="ulMenu">
+        <li class="nav-item">
+            <a href="#inicio">Inicio</a>
+        </li>
+        <li class="nav-item">
+            <a href="#sobre">Sobre</a>
+        </li>
+        <li class="nav-item">
+            <a href="#projetos">Projetos</a>
+        </li>
+        <li class="nav-item">
+            <a href="#contato">Contato</a>
+        </li>
+    </ul>
+  </div>
+   
+</nav>
